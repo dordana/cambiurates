832daaf91cf6fdde9524e5a2bc530d3d2a5650ab
Nikolay Ivanov <nikolay@zenlime.com>
2017-06-28 13:00:12 +0300

    cronjob fix

--- app/Console/Commands/Cambiu/Scripts/SyncRates.js
+++ app/Console/Commands/Cambiu/Scripts/SyncRates.js
@@ -23,12 +23,14 @@ var additionalParams = {
     }
 };
 
+require('dotenv').config();
+
 var mysql = require('mysql');
 var connection = mysql.createConnection({
-    host: '127.0.0.1',
-    user: 'root',
-    password: '1q2w3e4r',
-    database: 'cambiurates',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
     multipleStatements: 'true'
 });
 
