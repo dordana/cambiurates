7ef50105ab8d969f3247ab2bd0251432e8fc7f57
Dror <drorp24@gmail.com>
2017-07-18 15:50:16 +0300

    migration - making confirmation_code nullable

---
+++ database/migrations/2017_07_18_082908_update_users_allow_null_201707181539.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateUsersAllowNull201707181539 extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+         Schema::table(
+            'users', 
+            function (Blueprint $table) {
+                $table->string('confirmation_code')->nullable()->change();
+            });     
+     }
+    
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+    }
+}
