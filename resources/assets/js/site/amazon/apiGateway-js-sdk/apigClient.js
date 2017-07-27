7df477e5a22cedaedcbb2642d6cbf01053e59e83
Georgi Sokolov <georgi@zenlime.com>
2017-07-27 18:43:44 +0300

    change api url

--- resources/assets/js/site/amazon/apiGateway-js-sdk/apigClient.js
+++ resources/assets/js/site/amazon/apiGateway-js-sdk/apigClient.js
@@ -53,7 +53,8 @@ apigClientFactory.newClient = function (config) {
 
     
     // extract endpoint and path from url
-    var invokeUrl = 'https://cz471val2d.execute-api.us-west-2.amazonaws.com/production';
+    //var invokeUrl = 'https://cz471val2d.execute-api.us-west-2.amazonaws.com/production';
+   var invokeUrl = 'https://www.cambiu.com/api/v1/';
     var endpoint = /(^https?:\/\/[^\/]+)/g.exec(invokeUrl)[1];
     var pathComponent = invokeUrl.substring(endpoint.length);
 
